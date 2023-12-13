2f2fc69a40a81bee176a8568b9f6443fd059a971
Yan <yan.hourdebaigt.hiroux@gmail.com>
2023-12-13 11:15:21 +0100

    fonctions.h v1

--- fonctions.h
+++ fonctions.h
@@ -2,7 +2,15 @@
 #define __fonctions_H_
 
 
-
+int creerClient(char* nom, char* prenom, char* dateDeNaissance);
+int modifClient(char* idClient, int var, char* val);
+int suppClient(char* idClient);
+int affClient(char* idClient);
+int affPercingsClient(char* idClient);
+int ajouterPercing(char* idClient, char* date, int zone, int gaugeDePercage, int acte, char* nsGants, char* nsBijou, char* nsAiguille);
+int modifPering(char* idPercing, int var, char* val);
+int suppPercing(char* idPercing);
+int affPercing(char* idPercing);
 
 
 #endif
